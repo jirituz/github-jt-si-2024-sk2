a36e699270bba4a27aaa42a8e509f61c9ab37964
JiriTuz <jiri.tuz@tul.cz>
2024-10-10 13:31:49 +0200

    #1 základní program s možností opakování

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class program {
+
+    static void main(void v) {
+
+        // chci aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine(**********);
+            Console.WriteLine(**********);
+            Console.WriteLine(**********);
+            Console.WriteLine(**********); 
+
+
+            // vstup uživatele - To Do 
+
+            // logika pro výpis řady TO-DO
+
+            // opakování programu 
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a")
+            again = Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+}
