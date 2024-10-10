56d87c537cb3a4fdf06338df83f3bf74aa484824
JiriTuz <jiri.tuz@tul.cz>
2024-10-10 13:53:49 +0200

    #1 vstup hodnot a jejich výpis

--- Program.cs
+++ Program.cs
@@ -14,7 +14,38 @@ class program {
             Console.WriteLine(**********); 
 
 
-            // vstup uživatele - To Do 
+            // vstup uživatele - špatná varianta 
+            //Console.Write("Vlož první číslo řady: ");
+            //int first = int.Parse(Console.ReadLine());
+
+
+            // vstup uživatele lepší varianta 
+            Console.Write("Vlož první číslo řady (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celéčíslo): ");
+            }
+
+            Console.Write("Vlož poslední číslo řady (celé číslo): ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(), out last)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu poslední číslo řady (celéčíslo): ");
+            }
+
+            Console.Write("Vlož diferenci řady (celé číslo): ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(), out step)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu diferenci řady (celéčíslo): ");
+            }
+            
+            // Výpis uživatelského vstupu 
+            Console.WriteLine();
+            Console.WriteLine("=================");
+            Console.WriteLine("první číslo řady: {0}", first);
+            Console.WriteLine("poslední číslo řady", last);
+            Console.WriteLine("diferenci řady", step);
+            Console.WriteLine();
+            Console.WriteLine();
 
             // logika pro výpis řady TO-DO
 
